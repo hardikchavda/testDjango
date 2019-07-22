43c0584d8e79d7ae37e81c2e73de215c4a28ed00
Hardik Chavda <hardikkchavda@gmail.com>
2019-07-22 22:50:50 +0530

    Added Some Filters

--- blog1/urls.py
+++ blog1/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from django.contrib import admin
 from blog1 import views
 urlpatterns = [
-    url(r'^index/', views.index,name='index'),
-    url(r'^about/', views.about,name='about'),
+    url(r'^index/', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
 ]
